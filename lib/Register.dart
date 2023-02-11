017bb4d9a48ecf5a3da04f5a7c1b8ca1c9184b43
siddh0 <84409464+siddh0@users.noreply.github.com>
2023-02-11 17:24:35 +0530

    Add files via upload

---
+++ lib/Register.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'loginc.dart';
+
+class RegistrationUI extends StatelessWidget {
+  const RegistrationUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'lOGIN Ui',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: const Scaffold(
+          body: Registration(),
+        ));
+  }
+}
+
+class Registration extends StatelessWidget {
+  const Registration({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              const Text("Register",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.teal,
+                  )),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Enter Name",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.man),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Enter Email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Enter Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Confirm Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  gradient:
+                      const LinearGradient(colors: [Colors.blue, Colors.green]),
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Divider(
+                height: 30,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Have an Account?",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return const LoginUI();
+                            },
+                          ),
+                        );
+                      },
+                      child: const Text("Login"))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
