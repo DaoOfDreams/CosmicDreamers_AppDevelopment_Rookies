d180b41a65f40502f05413dcddcbe18229a72165
DaoOfDreams <114599782+DaoOfDreams@users.noreply.github.com>
2023-02-11 13:13:49 +0530

    Add files via upload

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'CustomerLogin.dart';
+import 'Auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,59 +14,63 @@ class _HomeState extends State<Home> {
 
   String button2 = 'CUSTOMER';
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutterApp'),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-          child: Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
+        appBar: AppBar(
+          title: const Text('FlutterApp'),
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+            child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
               image: DecorationImage(
-              image: AssetImage("assets/background.jfif"),
-              fit: BoxFit.cover,
-              )
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black),
-                    onPressed: () {},
-                    child: const Text('Who are you?')),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red),
-                    onPressed: () {
-                      setState(() {
-                        button1 = 'Proceed to login as Provider';
-                      });
-                    },
-                    child: Text(button1)),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
+            image: AssetImage("assets/background.jfif"),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('Who are you?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  )),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () {
+                    setState(() {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
                           builder: (BuildContext context) {
-                          return const LoginScreen();
+                            return const Auth();
                           },
-                          ),
-                        );
-                        button2 = 'Proceed to login as Customer';
-                      });
-                    },
-                    child: Text(button2)),
-              ],
-            ),
-          )
-      )
-    );
+                        ),
+                      );
+                      button1 = 'Proceed to login as Provider';
+                    });
+                  },
+                  child: Text(button1)),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const LoginScreen();
+                          },
+                        ),
+                      );
+                      button2 = 'Proceed to login as Customer';
+                    });
+                  },
+                  child: Text(button2)),
+            ],
+          ),
+        )));
   }
 }
